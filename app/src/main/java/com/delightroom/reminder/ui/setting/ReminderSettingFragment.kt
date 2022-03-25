2ff753f453161fd93540bf846be5eb4527d78306
ahn seokjoo <tjrwn0716@naver.com>
2022-03-25 14:50:49 +0900

    메인 화면 ui 작업 및 recyclerview 기본 틀 작성 (후에 edit page 설정하면서 추가 수정)

--- app/src/main/java/com/delightroom/reminder/ui/setting/ReminderSettingFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.delightroom.reminder.ui.setting
-
-import com.delightroom.reminder.R
-import com.delightroom.reminder.base.BaseFragment
-import com.delightroom.reminder.databinding.FragmentSettingReminderBinding
-
-class ReminderSettingFragment : BaseFragment<FragmentSettingReminderBinding>(R.layout.fragment_setting_reminder) {
-}

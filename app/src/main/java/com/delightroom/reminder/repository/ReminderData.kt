5978be4688ccd7e01acac5c52f94ef6b8d7aa9d3
ahn seokjoo <tjrwn0716@naver.com>
2022-03-29 22:20:23 +0900

    변수명 재수정, default ringtone 수정(부팅마다 uri가 바뀌어서 default ringtone으로 교체)

--- app/src/main/java/com/delightroom/reminder/repository/ReminderData.kt
+++ app/src/main/java/com/delightroom/reminder/repository/ReminderData.kt
@@ -1,13 +1,16 @@
 package com.delightroom.reminder.repository
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName = "ReminderList")
+@Entity(tableName = "ReminderList", indices = [Index(value = arrayOf("time"), unique = true)])
 @Parcelize
 data class ReminderData(
+    @ColumnInfo(name = "time")
     val time: String,
     val remind: String,
     var activate: Boolean,
